5b0245f17618a587398dc3a7b157ba9dc4054a75
naxareth <acephilipdenulan12@gmail.com>
2025-08-22 01:05:08 +0800

    Added sensor data endpoints

--- api/dbmsAPI/settings.py
+++ api/dbmsAPI/settings.py
@@ -27,7 +27,13 @@ DEBUG = True
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '10.0.2.2']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '10.0.2.2',
+    '192.168.0.12',  # Add your computer's IP address
+    '0.0.0.0',       # Allow all IPs (for development only)
+]
 
 
 # Application definition
