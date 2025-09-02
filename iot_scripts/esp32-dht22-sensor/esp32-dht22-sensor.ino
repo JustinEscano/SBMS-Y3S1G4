ea8cf8b19cb83722b8e73a3735cafbe2f191e34d
naxareth <acephilipdenulan12@gmail.com>
2025-09-02 23:51:48 +0800

    Merged LLM into updated branch

--- iot_scripts/esp32-dht22-sensor/esp32-dht22-sensor.ino
+++ iot_scripts/esp32-dht22-sensor/esp32-dht22-sensor.ino
@@ -4,13 +4,13 @@
 #include <DHT.h>
 
 // WiFi credentials - UPDATE THESE TO YOUR NETWORK
-const char* ssid = "";
-const char* password = "";
+const char* ssid = "SKYWORTH-FA13";
+const char* password = "281892655";
 
 // Django API endpoints - UPDATE YOUR_COMPUTER_IP
-const char* serverURL = "http://192.168.0.100:8000/api/esp32/sensor-data/";
-const char* healthURL = "http://192.168.0.100:8000/api/esp32/health/";
-const char* heartbeatURL = "http://192.168.0.100:8000/api/esp32/heartbeat/";
+const char* serverURL = "http://192.168.0.12:8000/api/esp32/sensor-data/";
+const char* healthURL = "http://192.168.0.12:8000/api/esp32/health/";
+const char* heartbeatURL = "http://192.168.0.12:8000/api/esp32/heartbeat/";
 
 // Device configuration - MUST MATCH Django equipment device_id
 const char* deviceID = "ESP32_001";
